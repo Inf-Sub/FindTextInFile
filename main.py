c344969d96a2e7ce008760091814f7c829a35cda
InfSub <99602461+Inf-Sub@users.noreply.github.com>
2025-02-16 14:14:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def find_text_in_file(file_path, search_text, search_number):
-    count_h = 0
+    count_char = 0
     with open(file_path, 'r', encoding='utf-8') as file:
         for line_number, line in enumerate(file, start=1):
             for position, char in enumerate(line):
                 if char == search_text:
-                    count_h += 1
-                    if count_h == search_number:
+                    count_char += 1
+                    if count_char == search_number:
                         print(f"157th '{search_text}' found at line {line_number}, position {position}")
                         return line_number, position
     
